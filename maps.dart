bef766f6774cf3cdb0b6799bcc31393e103516b4
fa-do-faker <uchihcl@gmail.com>
2024-11-02 22:46:13 -0300

    App de cadastro

---
+++ maps.dart
@@ -0,0 +1,43 @@
+import "dart:io";
+
+List<Map<String, dynamic>> cadastros = [];
+
+main() {
+  bool condicao = true;
+
+  print("\x1B[2J\x1B[0;0H");
+  while (condicao) {
+    print("=== DIGITE UM COMANDO ===");
+    String? comando = stdin.readLineSync();
+    if (comando == "sair") {
+      print("=== PROGRAMA FINALIZADO ===");
+      condicao = false;
+    } else if (comando == "cadastro") {
+      print("\x1B[2J\x1B[0;0H");
+      cadastrar();
+    } else if (comando == "imprimir") {
+      print(cadastros);
+    } else {
+      print("=== ESSE COMANDO NÃO EXISTE ===");
+    }
+  }
+}
+
+cadastrar() {
+  Map<String, dynamic> cadastro = {};
+
+  print("=== Digite o seu Nome ===");
+  cadastro["nome"] = stdin.readLineSync();
+
+  print("=== Digite sua Idade");
+  cadastro["idade"] = stdin.readLineSync();
+
+  print("=== Digite sua Cidade ===");
+  cadastro["cidade"] = stdin.readLineSync();
+
+  print("=== Digite seu Estado");
+  cadastro["estado"] = stdin.readLineSync();
+
+  cadastros.add(cadastro);
+
+}
